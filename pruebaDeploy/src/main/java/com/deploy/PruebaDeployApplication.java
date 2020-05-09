5975bbc7e1da1a2483d64c1cdc41220cbd9c2f75
jsebastiansarmiento92 <jsebastiansarmiento92@gmail.com>
2020-05-08 19:52:33 -0500

    primer

---
+++ pruebaDeploy/src/main/java/com/deploy/PruebaDeployApplication.java
@@ -0,0 +1,14 @@
+package com.deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PruebaDeployApplication extends SpringBootServletInitializer{
+
+//	public static void main(String[] args) {
+	//	SpringApplication.run(PruebaDeployApplication.class, args);
+	//}
+
+}
